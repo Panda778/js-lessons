6bd7bde57ca92ff2be7849d657a7324d27309204
Yan <artem200226dima18@gmail.com>
2023-05-12 21:04:18 +0900

    func validate input and save database in localStorage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,54 @@
 // btnModal - массив из элементов батн
 
-let btnModal = document.querySelectorAll('#btnmodal')
-let modals = document.querySelector('.modal-auth')
-
-btnModal.forEach(item => {
+const btnsModal = document.querySelectorAll('#btnmodal')
+const btnOut = document.querySelector('.button-out')
+const modals = document.querySelector('.modal-auth')
+const form = document.querySelector('#logInForm')
+const logIn = document.querySelector('#login')
+const password = document.querySelector('#password')
+const logInName = document.querySelector('#loginName')
+const modal_auth = document.querySelector('.modal-auth__dialog')
+// close open modal
+btnsModal.forEach(item => {
 	item.addEventListener('click', () => {
 		modals.classList.toggle('modal-open')
 	})
+})
+
+btnOut.addEventListener('click', () => {
+	btnOut.classList.add('none')
+	btnsModal[0].classList.remove('none')
+	logInName.innerHTML = ''
+	localStorage.removeItem('login')
+})
+
+// input login func
+function logins(e) {
+	e.preventDefault()
+
+	if (logIn.value.trim() !== '' && password.value.trim() !== '') {
+		let obj = {
+			name: logIn.value,
+			password: password.value,
+		}
+		localStorage.setItem('login', JSON.stringify(obj))
+		logIn.value = ''
+		password.value = ''
+		modals.classList.remove('modal-open')
+		btnsModal[0].classList.add('none')
+		btnOut.classList.remove('none')
+	} else {
+		alert('pliz zapolni polya')
+		logIn.value[0]
+		password.value = ''
+	}
+	let loginSucces = JSON.parse(localStorage.getItem('login'))
+	loginSucces
+		? (logInName.innerHTML = `Добро пожаловать : ${loginSucces.name}`)
+		: ''
+}
+
+form.addEventListener('submit', logins)
+modals.addEventListener('focus', e => {
+	console.log(e.type)
 })
